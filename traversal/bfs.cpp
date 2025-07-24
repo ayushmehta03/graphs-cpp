4bab46d954c51d95b0ca30e5b5dba0aeae43c342
Ayush Mehta <ayushmehtabps@gmail.com>
2025-07-24 08:15:48 +0530

    add

--- traversal/bfs.cpp
+++ traversal/bfs.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #include<queue>
 
 // space complexity is some what near around 0(3N)
-// time complexity is 0(N) for while loop for the queue and for thr FOR LOOP its 0(degree)
+// time complexity is 0(N) for while loop for the queue and for the FOR LOOP its 0(degree)
 // note-> degree is all the node connected to a particular node and to find total degree 
 // the formula is 2*edges
 
